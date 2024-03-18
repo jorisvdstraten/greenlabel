8cea207838d4a50bea2967b0cd68524f8d1699e8
Joris van der Straten <j.vanderstraten@fontys.nl>
2024-03-18 19:32:11 +0100

    Short demo

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import pandas as pd
+import requests as req
 
-st.title('Here should come some green labelling...')
+st.write('Here should come some green labelling...')
+
+# 53.333950289758135, 6.920911731760793
+url = "https://api.open-meteo.com/v1/forecast?latitude=53.33&longitude=6.9181&daily=temperature_2m_max"
+# read the data from url
+response = req.get(url)
+data = response.json()
+# convert the data to a pandas dataframe
+df = pd.DataFrame(data['daily']['temperature_2m_max'])
+# print the dataframe
+st.write(df)
