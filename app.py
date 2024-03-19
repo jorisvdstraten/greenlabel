20ba12cafc9044f391080bf10221a31745d7ca7d
Joris van der Straten <j.vanderstraten@fontys.nl>
2024-03-19 09:23:15 +0100

    First demonstration of using the open-meteo api to fetch weather data for a given location and time.

--- app.py
+++ app.py
@@ -2,14 +2,20 @@ import streamlit as st
 import pandas as pd
 import requests as req
 
-st.write('Here should come some green labelling...')
 
-# 53.333950289758135, 6.920911731760793
-url = "https://api.open-meteo.com/v1/forecast?latitude=53.33&longitude=6.9181&daily=temperature_2m_max"
-# read the data from url
-response = req.get(url)
-data = response.json()
-# convert the data to a pandas dataframe
-df = pd.DataFrame(data['daily']['temperature_2m_max'])
-# print the dataframe
-st.write(df)
+def weather_forecast(city_name, latitude, longitude):
+    st.write(f"Weather forecast for {city_name}:")
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=temperature_2m_max"
+    response = req.get(url)
+    data = response.json()
+    df = pd.DataFrame(data["daily"])
+    df["date"] = pd.to_datetime(df["time"])
+    df = df.drop("time", axis=1)
+    df = df.rename(columns={"temperature_2m_max": "temperature"})
+    # change column order
+    df = df[["date", "temperature"]]
+    st.bar_chart(df.set_index("date"))
+    st.write(df)
+
+weather_forecast("Delfzijl, Netherlands", 53.333950289758135, 6.920911731760793)
+weather_forecast("Madrid, Span", 40.415448970905786, -3.7018545480031992)
