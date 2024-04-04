b3ac53f8ef075bc28606dbdc1dab14e7f1390eee
Joris van der Straten <j.vanderstraten@fontys.nl>
2024-04-04 13:50:58 +0200

    Weather forecast is now based on daily sunshine hours and average wind speed.

--- functions.py
+++ functions.py
@@ -4,15 +4,18 @@ import requests as req
 
 def weather_forecast(city_name, latitude, longitude):
     st.write(f"Weather forecast for {city_name}:")
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=temperature_2m_max,sunshine_duration"
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=sunshine_duration,wind_speed_10m"
     response = req.get(url)
     data = response.json()
-    df = pd.DataFrame(data["daily"])
+    df = pd.DataFrame(data["hourly"])
     df["date"] = pd.to_datetime(df["time"])
     df["sunshine_duration"] = round(df["sunshine_duration"] / 3600, 1) # convert seconds to hours
     df = df.drop("time", axis=1)
-    df = df.rename(columns={"temperature_2m_max": "temperature"})
     # change column order
-    df = df[["date", "temperature", "sunshine_duration"]]
+    df = df[["date", "sunshine_duration", "wind_speed_10m"]]
+    # group all data by day and calculate the sum of sunshine_duration and the mean of wind_speed_10m
+    df = df.resample("D", on="date").agg({"sunshine_duration": "sum", "wind_speed_10m": "mean"}).reset_index()
+
+    # TODO: Change this bar chart because it does not work with the y-axis values
     st.bar_chart(df.set_index("date"))
     st.write(df)
