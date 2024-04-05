86f166d7e3aaad1e6a2635270182f3c293ed3183
eddiewouldgo03 <edouardlagesse43@gmail.com>
2024-04-05 12:51:04 +0200

    implement the pseudo-code

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+
 import streamlit as st
+
+#Import the function weather_forecast that we created from function
 from functions import weather_forecast
 
+#Display text on the top of the dashboard created 
 st.write("This will be our Green labelling model dashboard:")
-    
+
+#import and use the function weather_forecast that have been created. Use it for Amsterdam city.   
 weather_forecast("Amsterdam, Netherlands", 52.36826475460477, 4.895375012617035)
+
+#import and use the function weather_forecast that have been created. Use it for Madrid city. 
 weather_forecast("Madrid, Spain", 40.415448970905786, -3.7018545480031992)
--- functions.py
+++ functions.py
@@ -1,18 +1,44 @@
+
+#This library is to create interactive web applications for machine learning and data science  
 import streamlit as st
+
+#This library is to provides data structures and data analysis tools
 import pandas as pd
+        
+#This library simplifies HTTP requests, allowing to interact with APIs easily
 import requests as req
 
+
 def weather_forecast(city_name, latitude, longitude):
-    st.write(f"Weather forecast for {city_name}:")
+
+     #Display "Weather forecast for" and give the name of the city choosen in app.py
+    st.write(f"Weather forecast for {city_name}:") 
+
+    #Create the API URL to collectec data from open-meteo.com 
     url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=sunshine_duration,wind_speed_10m"
+
+    #request the url 
     response = req.get(url)
+
+    # Convert the response to JSON format
     data = response.json()
+
+    # Create a DataFrame from the JSON data
     df = pd.DataFrame(data["hourly"])
+
+    #change the format of the column to date time format 
     df["date"] = pd.to_datetime(df["time"])
-    df["sunshine_duration"] = round(df["sunshine_duration"] / 3600, 1) # convert seconds to hours
+
+    # convert seconds to hours
+    df["sunshine_duration"] = round(df["sunshine_duration"] / 3600, 1)
+
+    # Drop the 'time' column
     df = df.drop("time", axis=1)
+
+
     # change column order
     df = df[["date", "sunshine_duration", "wind_speed_10m"]]
+
     # group all data by day and calculate the sum of sunshine_duration and the mean of wind_speed_10m
     df = df.resample("D", on="date").agg({"sunshine_duration": "sum", "wind_speed_10m": "mean"}).reset_index()
 
