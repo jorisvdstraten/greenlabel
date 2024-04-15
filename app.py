b949e55218406c50c8a388752dcd65548c15f64e
Joris van der Straten <j.vanderstraten@fontys.nl>
2024-04-15 15:44:34 +0200

    Small example for organizing the code in a better way

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from functions import weather_forecast
+from functions import weather_forecastv2
 
 st.set_page_config(
     page_title="Greenlabel Dashboard",
@@ -9,7 +9,15 @@ st.set_page_config(
 
 st.markdown("<h1 style='text-align: center; color: green;'>Greenlabel Dashboard</h1>", unsafe_allow_html=True)
 
+# generate list of cities with lat and long
+cities = {
+    "Amsterdam, Netherlands": [52.36826475460477, 4.895375012617035],
+    "Madrid, Spain": [40.415448970905786, -3.7018545480031992],
+    "Paris, France": [48.8566969, 2.3514616],
+    "Berlin, Germany": [52.5170365, 13.3888599],
+    "Rome, Italy": [41.8933203, 12.4829321],    
+}
 
+# weather_forecast("Amsterdam, Netherlands", 52.36826475460477, 4.895375012617035, "Madrid, Spain", 40.415448970905786, -3.7018545480031992)
 
-weather_forecast("Amsterdam, Netherlands", 52.36826475460477, 4.895375012617035, "Madrid, Spain", 40.415448970905786, -3.7018545480031992)
-
+weather_forecastv2(cities)
--- functions.py
+++ functions.py
@@ -6,150 +6,209 @@ from datetime import datetime, timezone
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+import plotly.express as px
 
-def weather_forecast(city1_name, city1_latitude, city1_longitude, city2_name, city2_latitude, city2_longitude):
-    def fetch_weather_data(city_name, latitude, longitude): # Function to fetch and process weather data
-        url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=sunshine_duration,wind_speed_10m"
-        response = req.get(url)
-        data = response.json()
-        df = pd.DataFrame(data["hourly"])
-        df["date"] = pd.to_datetime(df["time"]).dt.date
-        df["sunshine_duration"] = round(df["sunshine_duration"] / 3600, 1)
-        df["wind_speed_10m"] = df["wind_speed_10m"].round(1)
-        df = df.drop("time", axis=1)
-        df = df.groupby("date").agg({"sunshine_duration": "sum", "wind_speed_10m": "mean"}).reset_index()
-        return df
+# Variables for the model
+P = 2250 # Define the values of the fixed variables for the energy calculation
+Cp = 0.6  
+Cf = 0.3  
+a = 20  # amount of wind turbines / wind mills
 
-    df_city1 = fetch_weather_data(city1_name, city1_latitude, city1_longitude) # Fetch weather data for both cities
-    df_city2 = fetch_weather_data(city2_name, city2_latitude, city2_longitude)
+y = 325
+q = 0.8
+n = 100 # amount of solar panels
 
-    plt.style.use("dark_background") # Set dark mode style for Matplotlib
-
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(13, 5)) # Create a single figure with two subplots (horizontally arranged)
-
-    bar_width = 0.4  # Plot wind speed comparison # Width of each bar
-    index = np.arange(len(df_city1))  # Index for x-axis positioning
-
-    ax1.bar(index - bar_width/2, df_city1["wind_speed_10m"], width=bar_width, color="skyblue", label=f"{city1_name} Wind Speed (m/s)")
-    ax1.bar(index + bar_width/2, df_city2["wind_speed_10m"], width=bar_width, color="orange", label=f"{city2_name} Wind Speed (m/s)")
-
-    for i, val in enumerate(df_city1["wind_speed_10m"]): # Annotate bars with actual values
-        ax1.text(i - bar_width/2, val + 0.1, f"{val:.1f}", ha="center", va="bottom", color="white", fontsize=8)
-    for i, val in enumerate(df_city2["wind_speed_10m"]):
-        ax1.text(i + bar_width/2, val + 0.1, f"{val:.1f}", ha="center", va="bottom", color="white", fontsize=8)
-
-    ax1.set_xlabel("Date", color="white")
-    ax1.set_ylabel("Wind Speed (m/s)", color="white")
-    ax1.set_title("Comparison of Wind Speed between Amsterdam & Madrid", color="white", pad=50)
-    ax1.set_xticks(index)
-    ax1.set_xticklabels(df_city1["date"], rotation=30)
-    ax1.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), facecolor="black")
-
-    ax2.bar(index - bar_width/2, df_city1["sunshine_duration"], width=bar_width, color="skyblue", label=f"{city1_name} Sunshine Duration (hours)") # Plot sunshine duration comparison
-    ax2.bar(index + bar_width/2, df_city2["sunshine_duration"], width=bar_width, color="orange", label=f"{city2_name} Sunshine Duration (hours)")
-
-    for i, val in enumerate(df_city1["sunshine_duration"]): # Annotate bars with actual values
-        ax2.text(i - bar_width/2, val + 0.1, f"{val:.1f}", ha="center", va="bottom", color="white", fontsize=8)
-    for i, val in enumerate(df_city2["sunshine_duration"]):
-        ax2.text(i + bar_width/2, val + 0.1, f"{val:.1f}", ha="center", va="bottom", color="white", fontsize=8)
-
-    ax2.set_xlabel("Date", color="white")
-    ax2.set_ylabel("Sunshine Duration (hours)", color="white")
-    ax2.set_title("Comparison of Sunshine Duration between Amsterdam & Madrid", color="white", pad=50)
-    ax2.set_xticks(index)
-    ax2.set_xticklabels(df_city1["date"], rotation=30)
-    ax2.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), facecolor="black")
-
-    st.pyplot(fig) # Display the plots using Streamlit
-
-    df_merged = pd.merge(df_city1, df_city2, on="date", suffixes=(f" ({city1_name})", f" ({city2_name})")) # Merge both dataframes based on the date
-
-    df_merged[f"wind_speed_difference_Amsterdam"] = (df_merged[f"wind_speed_10m ({city1_name})"] - df_merged[f"wind_speed_10m ({city2_name})"]).apply(lambda x: f"{x:.1f}") # Calculate difference columns
-    df_merged[f"sunshine_duration_difference_Amsterdam"] = (df_merged[f"sunshine_duration ({city1_name})"] - df_merged[f"sunshine_duration ({city2_name})"]).apply(lambda x: f"{x:.1f}")
-
-    df_merged = df_merged[["date", # Rearrange columns for better visualization
-                           f"wind_speed_10m ({city1_name})", f"wind_speed_10m ({city2_name})",
-                           f"wind_speed_difference_Amsterdam",
-                           f"sunshine_duration ({city1_name})", f"sunshine_duration ({city2_name})",
-                           f"sunshine_duration_difference_Amsterdam"]]
+v = 1.008
+enegetic_penalty = 10 # The energetic penalty is 1 Kwh for 100 GB
 
 
+def fetch_weather_data(city_name, latitude, longitude): # Function to fetch and process weather data
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=sunshine_duration,wind_speed_10m"
+    response = req.get(url)
+    data = response.json()
+    df = pd.DataFrame(data["hourly"])
+    df["date"] = pd.to_datetime(df["time"]).dt.date
+    df["sunshine_duration"] = round(df["sunshine_duration"] / 3600, 1)
+    df["wind_speed_10m"] = df["wind_speed_10m"].round(1)
+    df = df.drop("time", axis=1)
+    df = df.groupby("date").agg({"sunshine_duration": "sum", "wind_speed_10m": "mean"}).reset_index()
 
-    P = 2250 # Define the values of the fixed variables for the energy calculation
-    Cp = 0.6  
-    Cf = 0.3  
-    a = 20  # amount of wind turbines / wind mills
+    return df
 
-    y = 325
-    q = 0.8
-    n = 100 # amount of solar panels
+def weather_forecastv2(cities):
+    dfAll = pd.DataFrame()
+    # iterate over the cities and append df to dfAll
+    for city, coordinates in cities.items():
+        df = fetch_weather_data(city, coordinates[0], coordinates[1])
+        df["city"] = city
+        dfAll = pd.concat([dfAll, df])
 
-    v = 1.008
+    # calculate E(w) based on the provided equation
+    dfAll = dfAll.assign(Ew = lambda x: v * a * x['wind_speed_10m'])
+    # calculate E(s) based on the provided equation
+    dfAll = dfAll.assign(Es = lambda x: y * x['sunshine_duration'] * q * n / 1000)
+    # calculate total green energy
+    dfAll = dfAll.assign(Total_green_energy = lambda x: x['Ew'] + x['Es'])
+    # calculate the difference
+    # dfAll = dfAll.assign(Difference = lambda x: x['Total_green_energy'].diff())
 
-    enegetic_penalty = 10 # The energetic penalty is 1 Kwh for 100 GB
+    # show the dfAll
+    st.write(dfAll)
 
-    def calculate_wind_energy(row):  # Function to calculate E(w) based on the provided equation
-        t = row[f"wind_speed_10m ({city1_name})"]  # Get the wind speed value from city1
-        return round(v * a * t, 1) # (P * Cp * Cf * t * a / 1000, 1)
+    # plot a bar chart side by side for wind speed using plotly
+    fig = px.bar(dfAll, x='date', y='wind_speed_10m', color='city', barmode='group')
 
-    df_merged['E(w) / Amsterdam'] = df_merged.apply(calculate_wind_energy, axis=1) # Apply the function to create a new column 'E(w)' in the merged DataFrame
+    # # plot a bar chart side by side for sunshine using plotly
+    fig2 = px.bar(dfAll, x='date', y='sunshine_duration', color='city', barmode='group')
 
-    def calculate_wind_energy_2(row): # Function to calculate E(w) based on the provided equation
-        t = row[f"wind_speed_10m ({city2_name})"]  # Get the wind speed value from city1
-        return round(v * a * t, 1) # (P * Cp * Cf * t * a / 1000, 1)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write(fig)
 
-    df_merged['E(w) / Madrid'] = df_merged.apply(calculate_wind_energy_2, axis=1) # Apply the function to create a new column 'E(w)' in the merged DataFrame
+    with col2:
+        st.write(fig2)
+        
+# def weather_forecast(city1_name, city1_latitude, city1_longitude, city2_name, city2_latitude, city2_longitude):
+#     def fetch_weather_data(city_name, latitude, longitude): # Function to fetch and process weather data
+#         url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=sunshine_duration,wind_speed_10m"
+#         response = req.get(url)
+#         data = response.json()
+#         df = pd.DataFrame(data["hourly"])
+#         df["date"] = pd.to_datetime(df["time"]).dt.date
+#         df["sunshine_duration"] = round(df["sunshine_duration"] / 3600, 1)
+#         df["wind_speed_10m"] = df["wind_speed_10m"].round(1)
+#         df = df.drop("time", axis=1)
+#         df = df.groupby("date").agg({"sunshine_duration": "sum", "wind_speed_10m": "mean"}).reset_index()
+#         return df
+
+#     df_city1 = fetch_weather_data(city1_name, city1_latitude, city1_longitude) # Fetch weather data for both cities
+#     df_city2 = fetch_weather_data(city2_name, city2_latitude, city2_longitude)
+
+#     plt.style.use("dark_background") # Set dark mode style for Matplotlib
+
+#     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(13, 5)) # Create a single figure with two subplots (horizontally arranged)
+
+#     bar_width = 0.4  # Plot wind speed comparison # Width of each bar
+#     index = np.arange(len(df_city1))  # Index for x-axis positioning
+
+#     ax1.bar(index - bar_width/2, df_city1["wind_speed_10m"], width=bar_width, color="skyblue", label=f"{city1_name} Wind Speed (m/s)")
+#     ax1.bar(index + bar_width/2, df_city2["wind_speed_10m"], width=bar_width, color="orange", label=f"{city2_name} Wind Speed (m/s)")
+
+#     for i, val in enumerate(df_city1["wind_speed_10m"]): # Annotate bars with actual values
+#         ax1.text(i - bar_width/2, val + 0.1, f"{val:.1f}", ha="center", va="bottom", color="white", fontsize=8)
+#     for i, val in enumerate(df_city2["wind_speed_10m"]):
+#         ax1.text(i + bar_width/2, val + 0.1, f"{val:.1f}", ha="center", va="bottom", color="white", fontsize=8)
+
+#     ax1.set_xlabel("Date", color="white")
+#     ax1.set_ylabel("Wind Speed (m/s)", color="white")
+#     ax1.set_title("Comparison of Wind Speed between Amsterdam & Madrid", color="white", pad=50)
+#     ax1.set_xticks(index)
+#     ax1.set_xticklabels(df_city1["date"], rotation=30)
+#     ax1.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), facecolor="black")
+
+#     ax2.bar(index - bar_width/2, df_city1["sunshine_duration"], width=bar_width, color="skyblue", label=f"{city1_name} Sunshine Duration (hours)") # Plot sunshine duration comparison
+#     ax2.bar(index + bar_width/2, df_city2["sunshine_duration"], width=bar_width, color="orange", label=f"{city2_name} Sunshine Duration (hours)")
+
+#     for i, val in enumerate(df_city1["sunshine_duration"]): # Annotate bars with actual values
+#         ax2.text(i - bar_width/2, val + 0.1, f"{val:.1f}", ha="center", va="bottom", color="white", fontsize=8)
+#     for i, val in enumerate(df_city2["sunshine_duration"]):
+#         ax2.text(i + bar_width/2, val + 0.1, f"{val:.1f}", ha="center", va="bottom", color="white", fontsize=8)
+
+#     ax2.set_xlabel("Date", color="white")
+#     ax2.set_ylabel("Sunshine Duration (hours)", color="white")
+#     ax2.set_title("Comparison of Sunshine Duration between Amsterdam & Madrid", color="white", pad=50)
+#     ax2.set_xticks(index)
+#     ax2.set_xticklabels(df_city1["date"], rotation=30)
+#     ax2.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), facecolor="black")
+
+#     st.pyplot(fig) # Display the plots using Streamlit
+
+#     df_merged = pd.merge(df_city1, df_city2, on="date", suffixes=(f" ({city1_name})", f" ({city2_name})")) # Merge both dataframes based on the date
+
+#     df_merged[f"wind_speed_difference_Amsterdam"] = (df_merged[f"wind_speed_10m ({city1_name})"] - df_merged[f"wind_speed_10m ({city2_name})"]).apply(lambda x: f"{x:.1f}") # Calculate difference columns
+#     df_merged[f"sunshine_duration_difference_Amsterdam"] = (df_merged[f"sunshine_duration ({city1_name})"] - df_merged[f"sunshine_duration ({city2_name})"]).apply(lambda x: f"{x:.1f}")
+
+#     df_merged = df_merged[["date", # Rearrange columns for better visualization
+#                            f"wind_speed_10m ({city1_name})", f"wind_speed_10m ({city2_name})",
+#                            f"wind_speed_difference_Amsterdam",
+#                            f"sunshine_duration ({city1_name})", f"sunshine_duration ({city2_name})",
+#                            f"sunshine_duration_difference_Amsterdam"]]
+
+#     P = 2250 # Define the values of the fixed variables for the energy calculation
+#     Cp = 0.6  
+#     Cf = 0.3  
+#     a = 20  # amount of wind turbines / wind mills
+
+#     y = 325
+#     q = 0.8
+#     n = 100 # amount of solar panels
+
+#     v = 1.008
+
+#     enegetic_penalty = 10 # The energetic penalty is 1 Kwh for 100 GB
+
+#     def calculate_wind_energy(row):  # Function to calculate E(w) based on the provided equation
+#         t = row[f"wind_speed_10m ({city1_name})"]  # Get the wind speed value from city1
+#         return round(v * a * t, 1) # (P * Cp * Cf * t * a / 1000, 1)
+
+#     df_merged['E(w) / Amsterdam'] = df_merged.apply(calculate_wind_energy, axis=1) # Apply the function to create a new column 'E(w)' in the merged DataFrame
+
+#     def calculate_wind_energy_2(row): # Function to calculate E(w) based on the provided equation
+#         t = row[f"wind_speed_10m ({city2_name})"]  # Get the wind speed value from city1
+#         return round(v * a * t, 1) # (P * Cp * Cf * t * a / 1000, 1)
+
+#     df_merged['E(w) / Madrid'] = df_merged.apply(calculate_wind_energy_2, axis=1) # Apply the function to create a new column 'E(w)' in the merged DataFrame
 
 
-    def calculate_solar_energy(row): # Function to calculate E(s) based on the provided equation
-        z = row[f"sunshine_duration ({city1_name})"]  # Get the sunshine duration value from city1
-        return round(y * z * q * n / 1000, 1)
+#     def calculate_solar_energy(row): # Function to calculate E(s) based on the provided equation
+#         z = row[f"sunshine_duration ({city1_name})"]  # Get the sunshine duration value from city1
+#         return round(y * z * q * n / 1000, 1)
 
-    df_merged['E(s) / Amsterdam'] = df_merged.apply(calculate_solar_energy, axis=1) # Apply the function to create a new column 'E(s)' in the merged DataFrame
+#     df_merged['E(s) / Amsterdam'] = df_merged.apply(calculate_solar_energy, axis=1) # Apply the function to create a new column 'E(s)' in the merged DataFrame
 
-    def calculate_solar_energy_2(row): # Function to calculate E(s) based on the provided equation
-        z = row[f"sunshine_duration ({city2_name})"]  # Get the sunshine duration value from city1
-        return round(y * z * q * n / 1000, 1)
+#     def calculate_solar_energy_2(row): # Function to calculate E(s) based on the provided equation
+#         z = row[f"sunshine_duration ({city2_name})"]  # Get the sunshine duration value from city1
+#         return round(y * z * q * n / 1000, 1)
 
-    df_merged['E(s) / Madrid'] = df_merged.apply(calculate_solar_energy_2, axis=1) # Apply the function to create a new column 'E(s)' in the merged DataFrame
+#     df_merged['E(s) / Madrid'] = df_merged.apply(calculate_solar_energy_2, axis=1) # Apply the function to create a new column 'E(s)' in the merged DataFrame
 
-    df_merged['Total green energy Amsterdam'] = df_merged['E(w) / Amsterdam'] + df_merged['E(s) / Amsterdam']
-    df_merged['Total green energy Madrid'] = df_merged['E(w) / Madrid'] + df_merged['E(s) / Madrid']
+#     df_merged['Total green energy Amsterdam'] = df_merged['E(w) / Amsterdam'] + df_merged['E(s) / Amsterdam']
+#     df_merged['Total green energy Madrid'] = df_merged['E(w) / Madrid'] + df_merged['E(s) / Madrid']
 
-    df_merged["Difference"] = df_merged["Total green energy Madrid"] - df_merged["Total green energy Amsterdam"] - enegetic_penalty # Calculate difference and label
+#     df_merged["Difference"] = df_merged["Total green energy Madrid"] - df_merged["Total green energy Amsterdam"] - enegetic_penalty # Calculate difference and label
 
-    def calculate_label(value):
-        if value > 100:
-            return "yes"
-        elif value < -100:
-            return "no"
-        else:
-            return "possibly"
+#     def calculate_label(value):
+#         if value > 100:
+#             return "yes"
+#         elif value < -100:
+#             return "no"
+#         else:
+#             return "possibly"
 
-    df_merged["Label"] = df_merged["Difference"].apply(calculate_label)
+#     df_merged["Label"] = df_merged["Difference"].apply(calculate_label)
 
-    # st.write("Merged Weather Data with Energy Calculation:")
-    st.write(df_merged.set_index("date")) # Display the merged dataframe with the calculated energy column
+#     # st.write("Merged Weather Data with Energy Calculation:")
+#     st.write(df_merged.set_index("date")) # Display the merged dataframe with the calculated energy column
 
 
-    fig3, ax3 = plt.subplots(figsize=(4, 2)) # Set the desired figure size (width, height) in inches
+#     fig3, ax3 = plt.subplots(figsize=(4, 2)) # Set the desired figure size (width, height) in inches
 
-    label_colors = {'yes': 'green', 'no': 'red', 'possibly': 'orange'} # Define custom colors for labels
+#     label_colors = {'yes': 'green', 'no': 'red', 'possibly': 'orange'} # Define custom colors for labels
 
-    colors = [label_colors[label] for label in df_merged['Label'].unique()] # Create color palette using custom colors
+#     colors = [label_colors[label] for label in df_merged['Label'].unique()] # Create color palette using custom colors
 
-    sns.barplot(x='date', y='Difference', hue='Label', data=df_merged, ax=ax3, palette=colors) # Plotting the bar chart with labels
-    sns.set_context("talk")
+#     sns.barplot(x='date', y='Difference', hue='Label', data=df_merged, ax=ax3, palette=colors) # Plotting the bar chart with labels
+#     sns.set_context("talk")
 
-    df_merged = df_merged.reset_index(drop=True) # Reset index to ensure a valid sequential index starting from 0
+#     df_merged = df_merged.reset_index(drop=True) # Reset index to ensure a valid sequential index starting from 0
 
-    ax3.set_xlabel('Date', fontsize=6)
-    ax3.set_ylabel('Difference', fontsize=6)
-    ax3.set_title('Transfer labels for Amsterdam --> Madrid', fontsize=8)
-    ax3.set_xticklabels(df_merged['date'], rotation=30)  # Rotate x-axis labels for better readability
-    plt.tick_params(axis='x', labelsize=6)  # Font size for x-axis tick labels
-    plt.tick_params(axis='y', labelsize=6)  # Font size for y-axis tick labels
+#     ax3.set_xlabel('Date', fontsize=6)
+#     ax3.set_ylabel('Difference', fontsize=6)
+#     ax3.set_title('Transfer labels for Amsterdam --> Madrid', fontsize=8)
+#     ax3.set_xticklabels(df_merged['date'], rotation=30)  # Rotate x-axis labels for better readability
+#     plt.tick_params(axis='x', labelsize=6)  # Font size for x-axis tick labels
+#     plt.tick_params(axis='y', labelsize=6)  # Font size for y-axis tick labels
 
-    ax3.legend(title='Label', title_fontsize='6', loc='upper right', fontsize='6') # Customize legend and other settings
+#     ax3.legend(title='Label', title_fontsize='6', loc='upper right', fontsize='6') # Customize legend and other settings
 
-    st.pyplot(fig3) # Display the plot using Streamlit
+#     st.pyplot(fig3) # Display the plot using Streamlit
